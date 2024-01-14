9171d4aa0141977353e32a2ec36352eff6035c20
masum184e <masum184e@gmail.com>
2024-01-14 08:55:57 +0600

    optimization integrated

--- Sorting/bubble.cpp
+++ Sorting/bubble.cpp
@@ -2,11 +2,21 @@
 using namespace std;
 
 void bubble_sort(int arr[], int size){
-  for(int i=0;i<size-1;i++)
-    for(int j=0;j<size-i-1;j++)
-      if(arr[j]>arr[j+1])
+  for(int i=0;i<size-1;i++){
+    bool is_sorted = true;
+    for(int j=0;j<size-i-1;j++){
+      if(arr[j]>arr[j+1]){
         swap(arr[j], arr[j+1]);
+        is_sorted=false
+      }
+    }
+    if(is_sorted)break;
+  }
 }
+
+// is_sorted IS USED FOR TRACKING THE GIVEN ARRAY IS ALREADY SORTED OR NOT
+// IF THE GIVEN ARRAY IS ALREADY SORTED, THEN WE DON'T NEED TO SWAP
+// IF AT ONE ITERATION NO SWAP IS OCCURED, THAT MEANS WE GOT OUR SORTED ARRAY
 
 int main(){
   int size;
