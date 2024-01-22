ff33f440dc9dd4ea636d348cf16cfb485ad73e04
masum184e <masum184e@gmail.com>
2024-01-22 12:55:33 +0600

    dijkstra added

---
+++ Graph/dijkstra.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+
+void dijkstra(vector<vector<ll>> graph, ll source){
+    ll vertices=graph.size();
+
+    set<pair<ll,ll>> pq;
+    vector<ll> distance(vertices,LLONG_MAX);
+
+    pq.insert({0,source});
+    distance[source]=0;
+
+    while(!pq.empty()){
+        auto it=pq.begin();
+        ll node=it->second;
+        ll nodeDist=it->first;
+
+        pq.erase(it);
+
+        for(ll i=0;i<vertices;i++){
+            if(graph[node][i]!=INT_MAX && distance[node]+graph[node][i]<distance[i]){
+                pq.erase({distance[i],i});
+                distance[i]=distance[node]+graph[node][i];
+                pq.insert({distance[i],i});
+            }
+        }
+    }
+
+    cout<<endl<<"Minimum Distances From Source Node "<<source<<":"<<endl;
+    for(ll i=0;i<vertices;i++)
+        (distance[i]==LLONG_MAX)?cout<<"Vertex "<<i<<": Not Reachable"<<endl:cout<<"Vertex "<<i<<": "<<distance[i]<<endl;
+}
+
+int main(){
+    // ios_base::sync_with_stdio(false);cin.tie(NULL);
+    ll vertices, edges;
+
+    cout<<"Enter Number of Vertices and Edges: ";
+    cin>>vertices>>edges;
+
+    // INITIALIZING LOOP NODE 0 AND OTHER INFINITY
+    vector<vector<ll>> graph(vertices, vector<ll>(vertices, INT_MAX));
+    for(ll i=0;i<vertices;i++)graph[i][i]=0;
+
+    // GETTING INPUT
+    for(ll i=0;i<edges;i++){
+        ll from, to, weight;
+        cout<<"Enter "<<i+1<<"th Edges and Their Weights: ";
+        cin>>from>>to>>weight;
+        graph[from][to]=weight;
+    }
+
+    int source;
+    cout<<"Enter Source Vertex: ";
+    cin>>source;
+
+    // DISPLAYING GIVEN GRAPH
+    cout<<endl<<"Given Graph:"<<endl;
+    for(int i=0;i<vertices;i++){
+        for(int j=0;j<vertices;j++)
+        (graph[i][j]==INT_MAX)?cout<<"I"<<" ":cout<<graph[i][j]<<" ";
+        cout<<endl;
+    }
+
+    dijkstra(graph,source);
+
+    return 0;
+}
