c32beed76f0bbd6657c118c79d71f16fccbfe345
masum184e <masum184e@gmail.com>
2024-03-25 00:22:43 +0600

    recursive lcs added

--- Dynamic Programming/longest_common_subsequence.cpp
+++ Dynamic Programming/longest_common_subsequence.cpp
@@ -17,15 +17,29 @@ pair<string, int> lcs_memoization(string str1, string str2){
       arr[i][j]=0;
 
   // FILLING THE MEMOIZATOIN ARRAY WITH BOTTOM UP APPROACH AND GETTING THE LCS
-  string lcs_string;
   for(int i=1;i<=m;i++)
     for(int j=1;j<=n;j++)
       if(str1[i-1]==str2[j-1]){
-        lcs_string.push_back(str1[i-1]);
         arr[i][j]=arr[i-1][j-1]+1;
       }else{
         arr[i][j]=max(arr[i-1][j], arr[i][j-1]);
       }
+  
+  // Constructing the LCS string
+  string lcs_string;
+  int i=m,j=n;
+  while(i>0 && j>0) {
+      if(str1[i-1]==str2[j-1]){
+        lcs_string=str1[i-1]+lcs_string;
+        i--;
+        j--;
+      }else if(arr[i-1][j]>arr[i][j-1]){
+        i--;
+      }else {
+        j--;
+    }
+  }
+
 
   // DISPLAYING THE MEMOIZATION ARRAY
   cout<<"Memoization Table:"<<endl;
@@ -38,13 +52,26 @@ pair<string, int> lcs_memoization(string str1, string str2){
   return make_pair(lcs_string, arr[m][n]);
 }
 
-int lcs_recursive(string str1, string str2, int m, int n, string lcs_string){
+int lcs_length_recursive(string str1, string str2, int m, int n){
   if(m==0 || n==0)return 0;
   else if(str1[m-1]==str2[n-1]){
-    lcs_string+=str1[m-1];
-    return 1+lcs_recursive(str1, str2, m-1, n-1, lcs_string);
+    return 1+lcs_length_recursive(str1, str2, m-1, n-1);
   }
-  else return max(lcs_recursive(str1, str2, m-1, n, lcs_string), lcs_recursive(str1, str2, m, n-1, lcs_string));
+  else return max(lcs_length_recursive(str1, str2, m-1, n), lcs_length_recursive(str1, str2, m, n-1));
+}
+
+string lcs_string_recursive(string str1, string str2, int m, int n) {
+    if (m == 0 || n == 0)
+        return "";
+    else if (str1[m - 1] == str2[n - 1]) {
+        string lcs = lcs_string_recursive(str1, str2, m - 1, n - 1);
+        return lcs + str1[m - 1];
+    }
+    else {
+        string lcs1 = lcs_string_recursive(str1, str2, m - 1, n);
+        string lcs2 = lcs_string_recursive(str1, str2, m, n - 1);
+        return (lcs1.length() > lcs2.length()) ? lcs1 : lcs2;
+    }
 }
 
 int main(){
@@ -56,13 +83,13 @@ int main(){
   cout<<endl;
 
   pair<string, int> response = lcs_memoization(str1, str2);
-  cout<<"Length of LCS Using Memoization Method: "<<response.first<<endl;
-  cout<<"LCS using Memoization Method :"<<response.second<<endl;
+  cout<<"LCS using Memoization Method :"<<response.first<<endl;
+  cout<<"Length of LCS Using Memoization Method: "<<response.second<<endl;
 
   cout<<endl;
 
-  string lcs_string="";
-  cout<<"Length of LCS Using Recursion: "<<lcs_recursive(str1, str2, str1.length(), str2.length(), lcs_string)<<endl;
-  cout<<"LCS using Recursion: "<<lcs_string<<endl;
+  cout<<"Length of LCS Using Recursion: "<<lcs_length_recursive(str1, str2, str1.length(), str2.length())<<endl;
+  cout<<"LCS using Recursion: "<<lcs_string_recursive(str1, str2, str1.length(), str2.length())<<endl;
+  
   return 0;
 }
