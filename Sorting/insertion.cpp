0aa0ed05607b317c61ae8ef61421f2195da92fd0
masum184e <masum184e@gmail.com>
2024-08-29 17:33:42 +0600

    recursive insertion sort added

--- Sorting/insertion.cpp
+++ Sorting/insertion.cpp
@@ -1,6 +1,21 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+void recursive_insertion_sort(int arr[], int size){
+  if(size<=1)return;
+
+  recursive_insertion_sort(arr, size-1);
+  int item,j;
+  item=arr[size-1];
+  j=size-2;
+
+  while(j>=0 && arr[j]>item){
+    arr[j+1]=arr[j];
+    j--;
+  }
+  arr[j+1]=item;
+}
+
 void insertion_sort(int arr[], int size){
   for(int i=1;i<size;i++){
     int item,j;
