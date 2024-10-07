cd3d56f3aca254d18f829932ae21c8cca3eb2e21
masum184e <masum184e@gmail.com>
2024-10-07 10:05:51 +0600

    DLL added

---
+++ Linked List/doubly_linkedlist.cpp
@@ -0,0 +1,128 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long int
+
+struct Node {
+    int data;
+    Node* next;
+    Node* prev;
+};
+
+Node* createNode(int data){
+    Node* newNode = new Node();
+    newNode->data = data;
+    newNode->next = nullptr;
+    newNode->prev = nullptr;
+    return newNode;
+}
+
+Node* insertAtEnd(Node* head, int data){
+    Node* newNode = createNode(data);
+    if (head == nullptr) {
+        return newNode;
+    }
+
+    Node* temp = head;
+    while (temp->next != nullptr) {
+        temp = temp->next;
+    }
+
+    temp->next = newNode;
+    newNode->prev = temp;
+
+    return head;
+}
+
+Node* insertAtBeginning(Node* head, int data){
+    Node* newNode = createNode(data);
+
+    newNode->next = head;
+    head->prev = newNode;
+
+    return newNode;
+}
+
+Node* insertAtIndex(Node* head, int data, int index){
+    if(index<0){
+        cout<<"Invalid index."<<endl;
+        return head;
+    }
+
+    if(index==0)
+        return insertAtBeginning(head, data);
+
+    Node* newNode=createNode(data);
+    Node* temp = head;
+    while (index-- && temp->next != nullptr) {
+        temp = temp->next;
+    }
+
+    newNode->next = temp->next;
+    newNode->prev = temp;
+    
+    if (temp->next != nullptr) {
+        temp->next->prev = newNode;
+    }
+    
+    temp->next = newNode;
+
+    return head;
+}
+
+Node* reverseDLL(Node* head){
+    if(head==nullptr)return nullptr;
+
+    Node* temp = nullptr;
+    Node* current = head;
+    while (current != nullptr) {
+        temp = current->prev;
+        current->prev = current->next;
+        current->next = temp;
+
+        current = current->prev;
+    }    
+
+    if(temp!=nullptr)head=temp->prev;
+
+    return head;
+}
+
+void displayForward(Node* head){
+    Node* temp = head;
+    
+    while (temp != nullptr) {
+        cout << temp->data << " -> ";
+        temp = temp->next;
+    }
+    cout << "NULL" << endl;
+}
+
+void displayBackward(Node* head){
+    if (head == nullptr) return;
+
+    Node* temp = head;
+    while (temp->next != nullptr) {
+        temp = temp->next;
+    }
+
+    while (temp != nullptr) {
+        cout << temp->data << " -> ";
+        temp = temp->prev;
+    }
+    cout << "NULL" << endl;
+}
+
+int32_t main(){
+
+    Node* head = nullptr;
+    
+    head = insertAtEnd(head, 10);
+    head = insertAtEnd(head, 20);
+    head = insertAtEnd(head, 30);
+    head = insertAtBeginning(head, 5);
+
+    cout<<"Display Forward  : ";displayForward(head);
+    cout<<"Display Backward : ";displayBackward(head);
+
+    return 0;
+}
