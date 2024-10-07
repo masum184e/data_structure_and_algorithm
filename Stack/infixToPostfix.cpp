346ce0da677ca227fa1674601dd1c396805a2471
masum184e <masum184e@gmail.com>
2024-10-07 17:49:34 +0600

    infixToPostfix added

---
+++ Stack/infixToPostfix.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long int
+
+int precedence(char op) {
+    if (op == '+' || op == '-') return 1;
+    if (op == '*' || op == '/') return 2;
+    if (op == '^') return 3;
+    return 0;
+}
+
+bool isOperator(char c) {
+    return c == '+' || c == '-' || c == '*' || c == '/' || c == '^';
+}
+
+string infixToPostfix(string infix) {
+    stack<char> s;
+    string postfix;
+
+    for (char c : infix) {
+        if (isalnum(c)) {
+            postfix += c;
+        }else if (c == '(') {
+            s.push(c);
+        }else if (c == ')') {
+            while (!s.empty() && s.top() != '(') {
+                postfix += s.top();
+                s.pop();
+            }
+            s.pop();
+        }else if (isOperator(c)) {
+            while (!s.empty() && precedence(s.top()) >= precedence(c)) {
+                postfix += s.top();
+                s.pop();
+            }
+            s.push(c);
+        }
+    }
+
+    while (!s.empty()) {
+        postfix += s.top();
+        s.pop();
+    }
+
+    return postfix;
+}
+
+int32_t main(){
+
+    string infix;
+    cout << "Enter infix expression: ";
+    cin >> infix;
+
+    string postfix = infixToPostfix(infix);
+    cout << "Postfix expression: " << postfix << endl;
+
+    return 0;
+}
